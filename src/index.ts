bdda6e711e8717698e5d6e844f5aa6d12a91bea2
lucass <lucass@gmail.com>
2021-03-14 22:25:37 +0100

    Hello world

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const input:HTMLInputElement = (<HTMLInputElement>document.getElementById("bot-token"))
+const output:HTMLInputElement = (<HTMLInputElement>document.getElementById("bot-token-out"))
+const okButton:HTMLInputElement = (<HTMLInputElement>document.getElementById("bot-token-ok"))
+
+
+okButton.addEventListener("click", (e) => {
+    fetch("https://api.telegram.org/bot"+input.value+"/getMe").then(r => r.json()).then( v =>
+        output.textContent = JSON.stringify(v)
+    )
+})
