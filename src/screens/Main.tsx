b237e02be98c83c21c2a6bbb766e76953baab79c
lucass <lucass@gmail.com>
2021-03-31 00:38:44 +0200

    Plugable widgets

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,19 +1,94 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { FunctionComponent } from 'react'
-import BotToken from './settings/botToken/BotToken'
-import ReceivedMessages from './receivedMessages/ReceivedMessages'
-import Widget from './Widget'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import BotToken from './settings/botToken/BotToken';
+import ReceivedMessages from './receivedMessages/ReceivedMessages';
+import Widget from './Widget';
+import { Observable, Subject } from 'rxjs';
 
-const topWidget: JSX.Element = <BotToken />
-const bottomWidget: JSX.Element = <ReceivedMessages messages={["A", "B"]} />
+interface Input {
+    source: string;
+    sourceSubscriber: any;
+}
 
-const Main = () => ReactDOM.render(
-    <React.StrictMode>
-        <Widget component={topWidget} />
-        <Widget component={bottomWidget} />
-    </React.StrictMode>,
-    document.getElementById('main')
-)
+interface Connection {
+    name: string;
+    props?: Observable<any>;
+    renderer?: React.FunctionComponent;
+    inputs?: Input[];
+    output?: Observable<any>;
+}
+
+
+
+const hub:{[name: string]: Connection} = {}
+const allProps: {[name: string]: any} = {}
+
+const updateState: (name: string, newProps: any) => void  = (name, newProps) => {
+    allProps[name] = newProps
+    const rendered = Object.entries(hub).map(
+        ([key, value]) => {
+            if(value.renderer){
+                return value.renderer(allProps[key])
+            }
+        }
+    )
+    ReactDOM.render(
+        <React.StrictMode>
+            {rendered}
+        </React.StrictMode>,
+        document.getElementById('main')
+    )
+}
+
+const plug: (connection: Connection) => void = (connection) => {
+    if(connection.inputs){
+        for(let i of connection.inputs){
+            hub[i.source].output.subscribe(i.sourceSubscriber)
+        }
+    }
+    if(connection.props){
+        connection.props.subscribe((state: any) => {
+            updateState(connection.name, state)
+        })
+    }
+
+    hub[connection.name] = connection
+}
+
+const Main = () => {
+    const messages: Observable<any> = Observable.create((observer: any) => {
+        setInterval(() => {
+            observer.next("Hello")
+        }, 2000)
+    })
+
+    plug({
+        name: "Messages",
+        output: messages
+    } as Connection)
+    
+    const messagesLog: string[] = []
+    const messagesLogProps = new Subject()
+
+    plug({
+        name: "MessagesDisplay",
+        inputs: [{
+            source: "Messages",
+            sourceSubscriber: (msg: string) => {
+                messagesLog.push(msg)
+                messagesLogProps.next({
+                    "messages": messagesLog
+                })
+            }
+        } as Input],
+        props: messagesLogProps,
+        renderer: ReceivedMessages
+    } as Connection)
+
+    plug({
+        name: "BotToken",
+        renderer: BotToken
+    } as Connection)
+}
 
 export default Main
--- src/screens/receivedMessages/ReceivedMessages.tsx
+++ src/screens/receivedMessages/ReceivedMessages.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { FunctionComponent } from 'react';
 
-const ReceivedMessages:FunctionComponent<{ messages: Array<string> }> = ({messages}) => {    
+const ReceivedMessages:FunctionComponent<{ messages: string[] }> = ({messages}) => {    
     if(messages.length == 0) return <div id="ReceivedMessages"> <p>Sem mensagens</p> </div>
     const renderedMessages = messages.map( msg => (<p key={msg} className="receivedMessage">{msg}</p>) )
     return <div id="ReceivedMessages">
